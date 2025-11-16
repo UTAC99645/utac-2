c015e02ac644bdda9f65450a2a94cfea0218f99e
UTAC99645 <utac99645@gmail.com>
2025-11-16 08:18:03 +0800

    up

---
+++ src/main.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Hello UTAC V2.0</h1>
+    <button type="primary">Primary Button</button>
+  </div>
+</template>
+<script setup>
+import { ref } from 'vue';
+const count = ref(0);
+</script>
+
+<style scoped>
+</style>
