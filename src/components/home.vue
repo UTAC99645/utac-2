2c61ba603c10c65e917163414652d301a122f7c5
UTAC99645 <utac99645@gmail.com>
2026-02-01 14:52:52 +0000

    jjj

--- src/components/home.vue
+++ src/components/home.vue
@@ -4,17 +4,29 @@
     <ul>
       <li><button :class="[bing ? 'active' : '']" @click="() => { search_type = 'bing' }">bing</button></li>
       <li><button :class="[google ? 'active' : '']" @click="() => { search_type = 'google' }">google</button></li>
+      <li><button :class="[MarkDown ? 'active' : '']" @click="() => { search_type = 'MarkDown' }">MarkDown</button></li>
     </ul>
   </nav>
   <div class="page">
+    {{ search_type }}
+    {{ onMarkDown }}
     <h1 class="title">UTAC'S Sesrch</h1>
-    <div class="search-box">
+    <div v-if="!onMarkDown" class="search-box">
       <form class="form" @submit.prevent="searchfin">
-        <input v-model="searchText" type="text" id="Search" class="form__input" placeholder="" />
+        <input
+	v-model="searchText"
+	type="text"
+	id="Search"
+	class="form__input"
+	placeholder="" />
         <label for="Search" class="form__label">
           {{ search_type }}
         </label>
       </form>
+    </div>
+    <div v-else>
+      <button @click="() => { onMarkDown = false}">Back</button>
+      <Rader :url="searchText"/>
     </div>
   </div>
 </template>
@@ -23,6 +35,7 @@
 <script setup>
 import { onMounted, ref, watch } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
+import Rader from './MarkDown.vue'
 import "./home.css"
 import './addon/input.css'
 const search_link = ref("")
@@ -30,6 +43,8 @@ const searchText = ref('')
 const search_type = ref('google')
 const google = ref(true)
 const bing = ref(false)
+const MarkDown = ref(false)
+const onMarkDown = ref(false)
 const rute = useRoute()
 const roter = useRouter()
 
@@ -43,19 +58,30 @@ onMounted(() => {
 watch(search_type, [search_change, makeUrl], { immediate: true, deep: true })
 
 function searchfin() {
-  const query = encodeURIComponent(searchText.value);
-  window.open(`${search_link.value}${query}`, '_blank');
+  if (MarkDown.value) {
+  onMarkDown.value = true
+  return 0
+  }else {
+    const query = encodeURIComponent(searchText.value);
+    window.open(`${search_link.value}${query}`, '_blank');
+  }
 }
 
 function search_change() {
   if (search_type.value === 'google') {
     google.value = true
     bing.value = false
+    MarkDown.value = false
     search_link.value = "https://www.google.com/search?q="
   } else if (search_type.value === 'bing') {
     google.value = false
     bing.value = true
+    MarkDown.value = false
     search_link.value = "https://www.bing.com/search?q="
+  } else if (search_type.value === 'MarkDown') {
+    google.value = false
+    bing.value = false
+    MarkDown.value = true
   }
 }
 
@@ -64,15 +90,13 @@ function makeUrl() {
     roter.replace({ query: { type: 'google' } })
   } else if (search_type.value === 'bing') {
     roter.replace({ query: { type: 'bing' } })
+  } else if (search_type.value === 'MarkDown') {
+  roter.replace({ query: { type: 'MarkDown' } })
   }
 }
 
 function checkUrl() {
-  if (rute.query.type === 'google') {
-    search_type.value = 'google'
-  } else if (rute.query.type === 'bing') {
-    search_type.value = 'bing'
-  }
+  search_type.value = rute.query.type
 }
 </script>
 
