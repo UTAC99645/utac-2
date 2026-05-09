c8691976394524b40bc8683f4650023b87b740c7
UTAC99645 <UTAC99645@gmail.com>
2026-05-09 17:53:27 +0000

    a

--- src/main.vue
+++ src/main.vue
@@ -8,24 +8,24 @@
   -->
   <!-- ========================================================== -->
   <n-config-provider :theme="darkTheme">
-    <n-loading-bar-provider>
-      <n-message-provider>
+    <n-message-provider>
+      <n-loading-bar-provider>
 
         <!-- ---------- 面包屑导航 ---------- -->
         <n-flex justify="end">
           <n-breadcrumb separator="<->">
-            <n-breadcrumb-item
-              v-for="item in way"
-              :key="item.path"
-              @click="router.push(item.path)"
-            >
-              <n-text :type="route.path === item.path ? 'info' : 'default'">
+            <n-breadcrumb-item v-for="item in way" :key="item.path" @click="router.push(item.path)">
+              <n-text :type="route.name === item.name ? 'info' : 'default'">
                 {{ item.name }}
+              </n-text>
+            </n-breadcrumb-item>
+            <n-breadcrumb-item>
+              <n-text type="warn" @click="bgrdm">
+                Get A N BG
               </n-text>
             </n-breadcrumb-item>
           </n-breadcrumb>
         </n-flex>
-
         <!-- ---------- 分隔线 ---------- -->
         <n-divider />
 
@@ -34,8 +34,8 @@
           <router-view />
         </n-flex>
 
-      </n-message-provider>
-    </n-loading-bar-provider>
+      </n-loading-bar-provider>
+    </n-message-provider>
   </n-config-provider>
 </template>
 
@@ -50,17 +50,42 @@ import { darkTheme } from 'naive-ui';
 // ---------- Vue Router ----------
 import { useRoute, useRouter } from 'vue-router';
 
+import BGsrc from "./addition/Bg.way.json";
+import { onMounted, watch } from 'vue';
+
 // ============================================================
 // 导航数据
 // ============================================================
 
-const route  = useRoute();
+const route = useRoute();
 const router = useRouter();
 
 const way = ref([
-  { name: 'Home',  path: '/'       },
-  { name: 'About', path: '/about'  }
+  { name: 'Home', path: '/' },
+  { name: 'About', path: '/about' },
 ]);
+const BG = new Map(BGsrc)
+const BGU = computed(() => {
+  return BG.get(BGUN.value).url
+})
+const BGUN = ref("140342092_p0.jpg")
+
+onMounted(bgrdm)
+watch(BGUN, () => {
+  let N = `url("${BGU.value}")`
+  console.log(N)
+  document.documentElement.style.setProperty('--background-url', N);
+},
+  { immediate: true })
+
+function bgrdm() {
+  let key = Array.from(BG.keys())
+  let num = BG.size
+  let R = 0
+  R = Math.floor(Math.random() * num)
+  BGUN.value = key[R]
+}
+
 </script>
 
 <style scoped>
