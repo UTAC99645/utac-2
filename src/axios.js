4e5ec4e0641a617e541503bffd7580e267b1b82f
UTAC99645 <UTAC99645@gmail.com>
2026-05-09 04:34:41 +0000

    a

--- src/axios.js
+++ src/axios.js
@@ -6,16 +6,16 @@
  * ============================================================
  */
 
-// ---------- 响应拦截器 ----------
-// 在请求返回后，用当前时间减去请求开始时记录的时间戳，得到延迟
-axios.interceptors.response.use(res => {
-  res.delay = Date.now() - res.config.metadata.start;
-  return res;
-});
-
 // ---------- 请求拦截器 ----------
 // 在请求发出前，在 config.metadata 中记录当前时间戳
-axios.interceptors.request.use(config => {
-  config.metadata = { start: Date.now() };
+axios.interceptors.request.use((config) => {
+  config.metadata = { ...(config.metadata ?? {}), start: Date.now() };
   return config;
+});
+
+// ---------- 响应拦截器 ----------
+// 在请求返回后，用当前时间减去请求开始时记录的时间戳，得到延迟
+axios.interceptors.response.use((res) => {
+  res.delay = Date.now() - res.config.metadata.start;
+  return res;
 });
